e86269d89bd31c59ca58d134e1b233f3ff69af87
Heather Shelley <heathers@mediarain.com>
2014-12-09 23:14:51 -0700

    Added option for photo in membership directory template.

--- templates/directory.php
+++ templates/directory.php
@@ -1,5 +1,5 @@
 <?php if( !empty( $grouped_results) ): ?>
-<table>
+<table class="lg-directory-table">
 
 <?php foreach( $grouped_results as $key => $grouped_result ): ?>
 	
@@ -20,15 +20,17 @@
 			$name .= ( !empty ($member[0]['last_name'] ) ) ? ' ' . $member[0]['last_name'] : '';
 			$name = trim($name);
 			
-			$title = ( !empty ($member[0]['title'] ) ) ? '<br>' . $member[0]['title'] : '';
-			
 			$address = ( !empty ($member[0]['address'] ) ) ? $member[0]['address'] . '<br>' : '';
 			$address .= ( !empty ($member[0]['city'] ) ) ? $member[0]['city'] : '';
 			$address .= ( !empty ($member[0]['zip_code'] ) ) ? ', ' . $member[0]['zip_code'] : '';
 			
-			$phone = ( !empty ($member[0]['phone'] ) ) ? $member[0]['phone'] : '';
-			
-			$email = ( !empty ($member[0]['email'] ) ) ? '<a href="mailto:' . $member[0]['email'] . '">' . $member[0]['email'] . '</a>': '';
+			$member_link = '<a href="' . get_permalink() . '"';
+			if( !empty( $args['template_options']['member_link_attributes'] ) ) {
+				foreach( $args['template_options']['member_link_attributes'] as $key => $value ) {
+					$member_link .= ' ' . $key . '="' . $value . '"';
+				}
+			}
+			$member_link .= '>';
 		?>
 	
 		<tr>
@@ -38,31 +40,34 @@
 					<span class="<?php echo $field; ?>">
 					<?php 
 						switch($field) {
+							case 'photo': 
+								if( !empty( $member[0]['photo'] ) ) {
+									echo $member_link . wp_get_attachment_image( $member[0]['photo'], 'thumbnail' ) . '</a>';
+								}
+								break;
 							case 'name':
 								if( !empty( $member[0]['bio'] ) ) {
-									echo '<a href="' . get_permalink() . '"';
-									
-									if( !empty( $args['template_options']['member_link_attributes'] ) ) {
-										foreach( $args['template_options']['member_link_attributes'] as $key => $value ) {
-											echo ' ' . $key . '="' . $value . '"';
-										}
-									}
-									
-									echo '>' . $name . '</a>'; 
+									echo $member_link . $name . '</a>';	
 								}
 								else {
 									echo $name;
 								}
-								echo $title;
+								if ( !empty ($member[0]['title'] ) ) {
+									echo '<br>' . $member[0]['title'];
+								}
 								break;
 							case 'address':
 								echo $address;
 								break;
 							case 'phone':
-								echo $phone;
+								if ( !empty ($member[0]['phone'] ) ) {
+									echo $member[0]['phone'];
+								}
 								break;
 							case 'email':
-								echo $email;
+								if ( !empty ($member[0]['email'] ) ) {
+									echo '<a href="mailto:' . $member[0]['email'] . '">' . $member[0]['email'] . '</a>';
+								}
 								break;
 						}
 					?>
